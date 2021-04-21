500941ed0b5f7dc3b2a373589047c98430464edb
ramziddin_olimov <77951829+ogabekolimov@users.noreply.github.com>
2021-04-22 00:58:04 +0500

    carousel started javascript

--- main.js
+++ main.js
@@ -7,8 +7,32 @@ $(document).ready(function(){
         }
     })
 
+    // toggle menu/navbar script
+
     $('.menu-btn').click(function(){
         $('.navbar .menu').toogleClass("active");
         $('.menu-btn i').toogleClass("active");
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
     });
 }); 
